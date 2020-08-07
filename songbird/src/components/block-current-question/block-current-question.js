fdbabc02817fa27da103409a31fe76382ebe7cfb
cup0ra <cup0ra@users.noreply.github.com>
2020-08-07 19:25:04 +0300

    feat: add block current question

---
+++ songbird/src/components/block-current-question/block-current-question.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import bird from '../../assets/image/bird.jpg';
+
+
+export default class BlockCurrentQuestion extends Component {
+render(){
+
+    console.log('1')
+    const element = this.props.array
+    const image = this.props.plug === false ? bird : element.image;
+    const nameBird = this.props.plug === false ? '******' : element.name;
+    return (
+        <div className='block-current-question'>
+            <img className='block-current-question___img' src={ image } alt={ element.name}/>
+            <ul className='block-current-question__audio'>
+                <li className='block-current-question__name-bird'><h4>{nameBird}</h4></li>
+                <li>
+                   
+                </li>
+            </ul>
+        </div>
+    )
+}
+}
+    
+
