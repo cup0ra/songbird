35dd400d2f93a75c18622e6c1f400c381f0db1d5
cup0ra <cup0ra@users.noreply.github.com>
2020-08-07 20:40:36 +0300

    feat add button next round

---
+++ songbird/src/components/button-next-round/button-next-round.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ButtonNextRound extends Component{
+
+
+    render(){
+        const className = this.props.done ? 'btn btn-primary next-level' : 'btn btn-secondary next-level'
+        return (
+            <>
+                <button  type="button" className={className} onClick={() => this.props.update()}>Next level</button>
+            </>
+        )
+    }
+}
