ac806217988f915c155396ffff45668ec5b204e6
cup0ra <cup0ra@users.noreply.github.com>
2020-08-19 20:32:19 +0300

    feat: add gif vctory

--- songbird/src/components/block-victory/block-victory.js
+++ songbird/src/components/block-victory/block-victory.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import img from '../../assets/image/ho.gif'
 
-const BlockVictory = ({score}) => {
+const BlockVictory = ({score, restart}) => {
 return(
     <div className='jumbotron block-victory'>
         <h2 >Поздравляем!</h2>
         <p >Вы прошли викторину и набрали <b>{score}</b> из <b>30</b> возможных баллов</p>
-        <button  type="button" className="btn btn-primary next-level" >Попробовать еще раз! </button>
+        {score === 30 ?(
+        <img src={img} alt='image'/>
+        ) : (
+       <button  type="button" onClick={() => restart()} className="btn btn-primary next-level" >Попробовать еще раз! </button>
+        )}
+        
     </div>
 )
 }
