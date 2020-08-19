10988962026147c3b58a389e31c1d6d0eae8265c
cup0ra <cup0ra@users.noreply.github.com>
2020-08-19 20:21:37 +0300

    feat: add component progress-bar

--- songbird/src/components/audio-player/audio-player.js
+++ songbird/src/components/audio-player/audio-player.js
@@ -1,82 +1,54 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ReactAudioPlayer from 'react-audio-player';
+import ProgressBar from '../progress-bar/index'
+
 
 
 export default class AudioPlayer extends Component {
+    constructor(){
+        super()
+        this.audio = React.createRef()
+    }
     state = {
         isPlaying: true,
-        volume: '0.5',
-        duration: '0',
-        currentTime: '0',
-        timeValue: '0',
-        
-    }
-interval = () => {
-    let q
-    if(!this.isPlaying){
-       q =  setInterval(() => {
-            this.setState({
-                currentTime: String(Math.round(this.audioRef.currentTime)),
-                timeValue: String(Math.round(this.audioRef.currentTime))
-             })
-        
-        }, 1000);
-    } else {
-        clearInterval(q);
+        volume: '1',
+
     }
 
-}
     onPlay = () => {
-        this.audioRef.play();
-        this.setState(() => ({isPlaying: false}))
-        this.interval()
+        this.audio.current.play();
+        this.setState(() => ({isPlaying: false}));
+        
     }
     onPause = () => {
-        this.audioRef.pause();
-        this.setState(() => ({isPlaying: true})) 
+        this.audio.current.pause();
+        this.setState(() => ({isPlaying: true})); 
         
     }
     onChangeVolume = () => {
-        const volume = document.getElementById("volume")
-        this.setState(() => ({volume: volume.value}))
-        this.audioRef.volume=this.state.volume
-    }
-    onChangeTime = () => {
-        const time = document.getElementById("time");
-        this.setState(() => ({currentTime: time.value}))
-        this.audioRef.currentTime = time.value 
-        console.log(time.value)
+        const volume = this.InputVolume;
+        this.setState(() => ({volume: volume.value}));
+        this.audio.current.volume=this.state.volume;
     }
 
-    formatTime(seconds) {
-        const h = Math.floor(seconds / 3600)
-        const m = Math.floor((seconds % 3600) / 60)
-        const s = seconds % 60
-        return [h, m > 9 ? m : h ? '0' + m : m || '0', s > 9 ? s : '0' + s]
-            .filter(a => a)
-            .join(':')
-    }
 
-    componentDidMount() {
-        const audio = this.audioRef
-        
-        audio.onloadedmetadata = () => {
-            console.log('duration', this.audioRef.duration,this.audioRef.currentTime)
-            this.setState({
-               duration: String(Math.round(this.audioRef.duration)),
-               currentTime: String(Math.round(this.audioRef.currentTime)),
-               timeValue: String(Math.round(this.audioRef.currentTime)),
-            })
+    componentDidUpdate(prevProps) {
+        if (this.props.done !== prevProps.done) {
+            this.setState(() => ({isPlaying: true})); 
+            this.audio.current.pause();
         }
-    }
-
+        if (this.props.element !== prevProps.element) {
+            this.setState(() => ({isPlaying: true})); 
+            
+        }
+      }
+  
 render(){
-    console.log('render ')
+    console.log('render ', this.props.keys)
     return(
         <div className='audio-player'>
             
-        <audio id='audio' ref={(input) => {this.audioRef = input}} src={this.props.element.audio}  style={{ display: 'none' }} /> 
+        <audio key={this.props.keys} id='audio' ref={this.audio} src={this.props.element.audio}  style={{ display: 'none' }} /> 
             {this.state.isPlaying ? (
                 <button type="button" className="play" onClick={() => this.onPlay()} >          
                     <FontAwesomeIcon icon='play' />
@@ -86,13 +58,16 @@ render(){
                     <FontAwesomeIcon icon='pause' />
                 </button> 
                 )}
-                <div className='progress'>
-                    <span>{this.formatTime(this.state.currentTime)} </span>
-                    <input ref={(ref) => this.myInput = ref} type="range" className='time e-range' id="time" min='0'  max={this.state.duration}   value={this.state.currentTime} onChange={() => this.onChangeTime()}></input>
-                    <span>{this.formatTime(this.state.duration)} </span>
-                </div>
+                <ProgressBar audio={this.audio}  element={this.props.element}/>
                 <FontAwesomeIcon icon='volume-up' />
-                <input type="range" className='volume e-range' id="volume" name="volume"   min="0" max="1" step="0.1" onChange={() => this.onChangeVolume()}></input>
+                <input 
+                ref={(ref) => this.InputVolume = ref} 
+                type="range" 
+                className='volume e-range'  
+                name="volume"  
+                 min="0" 
+                 max="1" 
+                 step="0.1" onChange={() => this.onChangeVolume()}></input>
         </div>
     )
 }
---
+++ songbird/src/components/progress-bar/progress-bar.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+export default class ProgressBar extends Component {
+    state = {
+        duration: null,
+        currentTime: '0',
+        timeValue: '0', 
+    }
+    onChangeTime = () => {
+        const time = this.InputTime;
+        this.setState(() => ({currentTime: time.value}));
+        this.props.audio.current.currentTime = time.value; 
+    }
+
+    formatTime(seconds) {
+        const h = Math.floor(seconds / 3600)
+        const m = Math.floor((seconds % 3600) / 60)
+        const s = seconds % 60
+        return [h, m > 9 ? m : h ? '0' + m : m || '0', s > 9 ? s : '0' + s]
+            .filter(a => a)
+            .join(':')
+    }
+
+    componentDidMount() {
+        this.audio = this.props.audio.current;
+        this.audio.onloadedmetadata = (e) => {
+            this.setState({
+               duration: String(Math.round(e.target.duration)),
+               currentTime: String(Math.round(e.target.currentTime)),
+               timeValue: String(Math.round(e.target.currentTime)),
+            })
+        }
+       this.audio.onplay = (e) => {
+			this.currentTimeInterval = setInterval( () => {
+                this.InputTime.value = this.audio.currentTime;
+                this.setState({
+                    duration: String(Math.round(e.target.duration)),
+                    currentTime: String(Math.round(e.target.currentTime)),
+                    timeValue: String(Math.round(e.target.currentTime)),
+                 })
+            }, 500);
+        };
+        this.audio.onpause = () => {
+			clearInterval(this.currentTimeInterval);
+        };
+        this.audio.onended = () => {
+            this.setState(() => ({
+                currentTime: '0',
+                timeValue: '0',
+            })); 
+        }
+    }
+    componentWillUnmount() {
+        clearInterval(this.currentTimeInterval);
+        this.setState = (state,callback)=>{
+            return;
+        };
+      
+      }
+    
+      componentDidUpdate(prevProps) {
+        if (this.props.element !== prevProps.element) {
+            clearInterval(this.currentTimeInterval);
+        }
+      }
+render(){
+    return(
+    <div className='progress'>
+        <span>{this.formatTime(this.state.currentTime)} </span>
+        <input 
+        ref={(ref) => this.InputTime = ref} 
+        type="range" 
+        className='time e-range'  
+        min='0'  
+        max={this.state.duration}   
+        value={this.state.currentTime} onChange={() => this.onChangeTime()}></input>
+        <span>{this.formatTime(this.state.duration)} </span>
+    </div>
+    )
+}
+}
